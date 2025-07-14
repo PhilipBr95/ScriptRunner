85cf858d40591f12855c837f2b4f95a6239931ab
Philip Bridges <philipbr95@gmail.com>
2025-07-14 21:06:43 +0100

    Added some examples

---
+++ Examples/Randon Sql with button/Fix1.sql
@@ -0,0 +1,5 @@
+--Use this structure if you need to reference the variable
+DECLARE @Name@ varchar(50)
+DECLARE @MemberNumberVariable varchar(50) = '{Name}'
+
+select data as [Filenames], data as [Filenames2], data as [Filenames3], data as [Filenames4], data as [Filenames5] from PK WHERE data like '%{Name}%'
